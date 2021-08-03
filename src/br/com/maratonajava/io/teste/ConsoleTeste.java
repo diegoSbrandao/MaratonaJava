8c752525e19bac489dbe343dab2ba3daf876f2ff
dsb88 <diego.brandao@outlook.com>
2021-08-03 15:00:57 -0300

    Exercicios NIO(Path,Paths,File,Normalização,Relative)

---
+++ src/br/com/maratonajava/io/teste/ConsoleTeste.java
@@ -0,0 +1,25 @@
+package br.com.maratonajava.io.teste;
+
+import java.io.Console;
+
+public class ConsoleTeste {
+    public static void main(String[] args) {
+//        Console c = System.console();
+//        char[] pw = c.readPassword("%s", "pw: ");
+//        for (char pass : pw) {
+//            c.format("%c ", pass);
+//        }
+//        c.format("\n");
+//        String texto;
+//        while (true) {
+//            texto = c.readLine("%s", "Digite: ");
+//            c.format("esse texto %s foi digitado", retorno(texto));
+//        }
+//    }
+//
+//    public static String retorno(String arg1) {
+//        return arg1;
+//    }
+//}
+    }
+}
---
+++ src/br/com/maratonajava/nio/teste/NormalizacaoTeste.java
@@ -0,0 +1,19 @@
+package br.com.maratonajava.nio.teste;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizacaoTeste {
+    public static void main(String[] args) {
+//        String diretorioProjeto = "JavaOnline\\home\\william\\dev";
+//        String arquivoTxt = "..\\..\\arquivo.txt";
+//        Path p1 = Paths.get(diretorioProjeto, arquivoTxt);
+//        System.out.println(p1);
+//        System.out.println(p1.normalize());
+//        Path p2 = Paths.get("a/./b/./c");
+//        System.out.println(p2);
+//        System.out.println(p2.normalize());
+//    }
+}
+
+}
